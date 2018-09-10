dbecd7bba8b97ee1451538ee005ce24a79848b59
piarovira <piarovira98@gmail.com>
2018-09-10 20:29:03 -0300

    registroRestaurante

--- src/registroRestaurante/Main.java
+++ src/registroRestaurante/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package registroRestaurante;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 716, 497));
+        Parent root = FXMLLoader.load(getClass().getResource("registroRestaurante.fxml"));
+        primaryStage.setTitle("Solicitud de registro");
+        primaryStage.setScene(new Scene(root, 371, 313));
         primaryStage.show();
     }
 
---
+++ src/registroRestaurante/RegistroRestaurante.java
@@ -0,0 +1,4 @@
+package registroRestaurante;
+
+public class RegistroRestaurante {
+}
