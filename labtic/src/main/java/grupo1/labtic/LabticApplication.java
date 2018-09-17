452620e68c11af56281f8c605a46e9b6fb67dc8b
Juan Pedro Grille <jopegrille.jg@gmail.com>
2018-09-17 17:20:56 -0300

    Creamos el proyecto principal

---
+++ labtic/src/main/java/grupo1/labtic/LabticApplication.java
@@ -0,0 +1,12 @@
+package grupo1.labtic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabticApplication.class, args);
+    }
+}
---
+++ labtic/src/test/java/grupo1/labtic/LabticApplicationTests.java
@@ -0,0 +1,16 @@
+package grupo1.labtic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabticApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
