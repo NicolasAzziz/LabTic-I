c66aab4e6c8cd7a4b3444a983447b7accf3e1ed7
piarovira <piarovira98@gmail.com>
2018-09-19 01:19:41 -0300

    cosas

--- labtic/src/main/java/grupo1/labtic/ui/restaurants/SolicitarDatos.java
+++ labtic/src/main/java/grupo1/labtic/ui/restaurants/SolicitarDatos.java
@@ -3,16 +3,15 @@ package grupo1.labtic.ui.restaurants;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 
 public class SolicitarDatos {
     @FXML
-    private ChoiceBox<CheckMenuItem> metodosPago;
+    private ChoiceBox metodosPago;
 
-    ObservableList list = FXCollections.observableArrayList();
+    ObservableList listaMetodosPago = FXCollections.observableArrayList();
 
     @FXML
     public void initialize(){
@@ -20,14 +19,14 @@ public class SolicitarDatos {
     }
 
     public void loadData(){
-        list.removeAll(list);
+        listaMetodosPago.removeAll(listaMetodosPago);
         CheckMenuItem tarjetaD = new CheckMenuItem("Tarjeta de débito");
         CheckMenuItem tarjetaC = new CheckMenuItem("Tarjeta de crédito");
         CheckMenuItem efectivo = new CheckMenuItem("Efectivo");
         CheckMenuItem tAlimentacion = new CheckMenuItem("Ticket de alimentación");
         CheckMenuItem tRestaurante = new CheckMenuItem("Ticket restaurante");
-        list.addAll(tarjetaC, tarjetaD, efectivo, tAlimentacion, tRestaurante);
-        metodosPago.getItems().addAll(list);
+        listaMetodosPago.addAll(tarjetaD, tarjetaC, efectivo, tAlimentacion, tRestaurante);
+        metodosPago.getItems().addAll(listaMetodosPago);
     }
 
     public void desplegarMetodosDePago(MouseEvent mouseEvent) {
