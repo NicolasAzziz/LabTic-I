61b4c20172380bd15789a3519b0bb4d898869446
piarovira <piarovira98@gmail.com>
2018-09-21 19:17:04 -0300

    intento de registro

--- labtic/src/main/java/grupo1/labtic/ui/restaurants/SolicitarDatos.java
+++ labtic/src/main/java/grupo1/labtic/ui/restaurants/SolicitarDatos.java
@@ -5,29 +5,53 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import org.springframework.stereotype.Component;
 
+import java.awt.event.ActionEvent;
 
+@Component
 public class SolicitarDatos {
-//    @FXML
-//    private ChoiceBox<CheckBox> metodosPago;
-//
-//    ObservableList listaMetodosPago = FXCollections.observableArrayList();
-//
-//    @FXML
-//    public void initialize(){
-//        loadData();
-//    }
-//
-//    public void loadData(){
-//        listaMetodosPago.removeAll(listaMetodosPago);
-//        CheckBox tarjetaD = new CheckBox("Tarjeta de débito");
-//        CheckBox tarjetaC = new CheckBox("Tarjeta de crédito");
-//        CheckBox efectivo = new CheckBox("Efectivo");
-//        CheckBox tAlimentacion = new CheckBox("Ticket de alimentación");
-//        CheckBox tRestaurante = new CheckBox("Ticket restaurante");
-//        listaMetodosPago.addAll(tarjetaD, tarjetaC, efectivo, tAlimentacion, tRestaurante);
-//        metodosPago.getItems().addAll(listaMetodosPago);
-//    }
+    @FXML
+    private TextField nombreRestaurante;
+    @FXML
+    private TextField telefonoRestaurante;
+    @FXML
+    private TextField direccionRestaurante;
+    @FXML
+    private TextField barrioRestaurante;
+    @FXML
+    private TextField hAperturaRestaurante;
+    @FXML
+    private TextField mAperturaRestaurante;
+    @FXML
+    private TextField hCierreRestaurante;
+    @FXML
+    private TextField mCierreRestaurante;
+    @FXML
+    private TextArea descR;
+    @FXML
+    private TextField webRestaurante;
+    @FXML
+    private TextField nMesasRestaurante;
+    @FXML
+    private MenuButton metodosPagoMenu;
+    @FXML
+    private MenuButton comidasMenu;
+    @FXML
+    private CheckMenuItem tarjetaD;
+    @FXML
+    private CheckMenuItem tarjetaC;
+    @FXML
+    private CheckMenuItem efectivo;
+    @FXML
+    private CheckMenuItem ticketA;
+    @FXML
+    private CheckMenuItem ticketR;
+
+    @FXML
+    public void registrar(ActionEvent event){
+        System.out.print("hola");
+    }
 
     public void desplegarMetodosDePago(MouseEvent mouseEvent) {
     }
