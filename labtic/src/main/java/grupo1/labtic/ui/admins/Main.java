6ab1a14bd12c7695fdc86189937da4c0da39367c
Federico Abdo <fabdo@correo.um.edu.uy>
2018-09-24 17:23:54 -0300

    Solucion error de driver mysql

--- labtic/src/main/java/grupo1/labtic/ui/admins/Main.java
+++ labtic/src/main/java/grupo1/labtic/ui/admins/Main.java
@@ -6,14 +6,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class Main extends Application {
 
+
+    private AnnotationConfigApplicationContext context;
+    private Parent root;
+
     public void start(Stage primaryStage) throws Exception{
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LabticApplication.class);
-        Parent root = FXMLLoader.load(getClass().getResource("admin.fxml"));
-        primaryStage.setTitle("admin");
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LabticApplication.class);
+//        Parent root = FXMLLoader.load(getClass().getResource("admin.fxml"));
+//        primaryStage.setTitle("admin");
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
+
+        context = new AnnotationConfigApplicationContext(Main.class);
+
+        FXMLLoader loader = new FXMLLoader(Administrar.class.getResource("admin.fxml"));
+        loader.setControllerFactory(context::getBean);
+
+        root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
