fe66de022e5266437cc532f29ceaab3ec390435b
Juan Pedro Grille <jopegrille.jg@gmail.com>
2018-11-04 20:45:37 -0300

    Modifique la pantalla principal de restaurant

--- labtic/src/main/java/grupo1/labtic/ui/restaurants/RestaurantePrincipal.java
+++ labtic/src/main/java/grupo1/labtic/ui/restaurants/RestaurantePrincipal.java
@@ -1,11 +1,24 @@
 package grupo1.labtic.ui.restaurants;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import org.springframework.stereotype.Component;
-
-
 
 @Component
 public class RestaurantePrincipal {
+
+    @FXML
+    private TableView<?> reservasActivas;
+    @FXML
+    private TableView<?> reservasPendientes;
+    @FXML
+    private TableColumn nombre;
+    @FXML
+    private TableColumn nroMesa;
+    @FXML
+    private TableColumn hora;
+
 
 
 }
